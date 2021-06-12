b58eecf781ab372a2b48ce863e295f419cb2a200
ShiroKatake <tgkhang262@gmail.com>
2021-06-12 14:10:44 +1000

    UPDATE: Added unstick timer and tag checking

--- GMTK Jam 2021/Assets/Scripts/Cube.cs
+++ GMTK Jam 2021/Assets/Scripts/Cube.cs
@@ -5,29 +5,44 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     [SerializeField] private float stickTime = 5f;
+    [SerializeField] private float unstickTime = 2f;
+	
 	private Timer stickTimer;
+	private Timer unstickTimer;
+	private bool canStick;
 
 	private void Awake()
 	{
+		unstickTimer = Timer.CreateComponent(gameObject, "Unstick Time", unstickTime);
 		stickTimer = Timer.CreateComponent(gameObject, "Stick Time", stickTime);
 	}
 
 	private void Update()
 	{
-		if (stickTimer.TimerFinished)
+		if (unstickTimer.TimerFinished)
 		{
+			canStick = true;
 			Debug.Log("Unstick");
 		}
+
+		if (stickTimer.TimerFinished)
+			unstickTimer.StartTimer();
 	}
 
 	protected virtual void PartialStick(float stickTime) {
-		Debug.Log("Stick!");
-		stickTimer.StartTimer();
+		if (canStick)
+		{
+			Debug.Log("Stick!");
+			stickTimer.StartTimer();
+			canStick = false;
+		}
 	}
+
     protected virtual void ActivateAbility() {}
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		PartialStick(stickTime);
+		if (collision.gameObject.tag== "Cube")
+			PartialStick(stickTime);
 	}
 }
