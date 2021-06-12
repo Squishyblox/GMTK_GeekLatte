deee81ce517c586cc3ba6c738d5cff393915c483
ShiroKatake <tgkhang262@gmail.com>
2021-06-12 13:19:05 +1000

    ADD: Abstract Cube object

---
+++ GMTK Jam 2021/Assets/Scripts/Cube.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Cube : MonoBehaviour
+{
+    private float stickTimer;
+
+    protected void PartialStick() {}
+    protected void ActivateAbility() {}
+}
