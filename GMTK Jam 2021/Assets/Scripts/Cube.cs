7f61eb91c4bf5f4138ac3c3a1b427cb1ee85d3ac
DaivyIsHere <nyannah233@gmail.com>
2021-06-12 14:24:04 +0800

    UPDATE : Recently used scene.

--- GMTK Jam 2021/Assets/Scripts/Cube.cs
+++ GMTK Jam 2021/Assets/Scripts/Cube.cs
@@ -6,65 +6,62 @@ public class Cube : MonoBehaviour
 {
     [SerializeField] private float stickTime = 5f;
     [SerializeField] private float unstickTime = 2f;
-	
-	private Timer stickTimer;
-	private Timer unstickTimer;
-	private bool canStick = true;
-	private float timePassed;
 
-	private void Awake()
+    private bool canStick = true;
+	private float speed = 10;
+
+    private void Start()
+    {
+        canStick = true;
+    }
+
+	void Update() 
 	{
-		unstickTimer = Timer.CreateComponent(gameObject, "Unstick Time", unstickTime);
-		stickTimer = Timer.CreateComponent(gameObject, "Stick Time", stickTime);
+		//super dirty. Will delete it later
+		float hori = Input.GetAxisRaw("Horizontal");
+		float ver = Input.GetAxisRaw("Vertical");
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(hori, ver)* speed * Time.deltaTime, ForceMode2D.Impulse);	
 	}
 
-	private void Start()
-	{
-		canStick = true;
-	}
+    protected virtual IEnumerator PartialStick(Rigidbody2D rb, Collision2D collision)
+    {
+        Stick(rb, collision);
+        yield return new WaitForSeconds(stickTime);
+        Unstick(rb);
+        yield return new WaitForSeconds(unstickTime);
+        yield return 0;
+    }
 
-	protected virtual IEnumerator PartialStick(Rigidbody rb, Collision collision) {
-		Stick(rb, collision);
-		yield return new WaitForSeconds(stickTime);
-		Unstick(rb);
-		yield return new WaitForSeconds(unstickTime);
-		yield return 0;
-	}
+    protected virtual void ActivateAbility() { }
 
-    protected virtual void ActivateAbility() {}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (canStick && collision.gameObject.tag == "Cube")
+        {
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            StartCoroutine(PartialStick(rb, collision));
+        }
+    }
 
-	private void OnCollisionEnter(Collision collision)
-	{
-		if (canStick && collision.gameObject.tag == "Cube") {
-			Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
-			StartCoroutine(PartialStick(rb, collision));
-		}
-	}
+    private void Stick(Rigidbody2D rb, Collision2D collision)
+    {
+        Debug.Log("Stick!");
+        // creates joint
+        FixedJoint2D joint = gameObject.AddComponent<FixedJoint2D>();
+        // sets joint position to point of contact
+        joint.anchor = collision.contacts[0].point;
+        // conects the joint to the other object
+        joint.connectedBody = collision.transform.GetComponentInParent<Rigidbody2D>();
+        // Stops objects from continuing to collide and creating more joints
+        joint.enableCollision = false;
+        canStick = false;
+    }
 
-	private void Stick(Rigidbody rb, Collision collision)
-	{
-		Debug.Log("Stick!");
-
-		// creates joint
-		FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-		// sets joint position to point of contact
-		joint.anchor = collision.contacts[0].point;
-		// conects the joint to the other object
-		joint.connectedBody = collision.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>();
-		// Stops objects from continuing to collide and creating more joints
-		joint.enableCollision = false;
-
-		//rb.transform.parent = Player.Instance.transform;
-		//Debug.Log(rb.transform.parent.name);
-		//rb.constraints = RigidbodyConstraints.FreezeAll;
-		canStick = false;
-	}
-
-	private void Unstick(Rigidbody rb)
-	{
-		Debug.Log("Unstick");
-		//rb.transform.parent = null;
-		//rb.constraints = RigidbodyConstraints.None;
-		canStick = true;
-	}
+    private void Unstick(Rigidbody2D rb)
+    {
+        Debug.Log("Unstick");
+        //rb.transform.parent = null;
+        //rb.constraints = RigidbodyConstraints.None;
+        canStick = true;
+    }
 }
